4f72acd1291dc9b6b6b1788c65e2d3d9cebf8b80
Sukk4 <jukkaxkari@gmail.com>
2017-03-15 03:51:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+function generateGearStr() {
+
+  const itemType = document
+    .getElementsByClassName("main-precontents")[0]
+    .getElementsByClassName("breadcrumb")[0]
+    .lastElementChild
+    .childNodes[0]
+    .innerHTML
+    .toLowerCase();
+
+  // example array  ["", "item=123213", "itemname&bonus=1233:123:302"]  
+  const urlLocArr = window
+    .location
+    .pathname
+    .split("/");
+
+  const itemId = urlLocArr[1].substr(5, urlLocArr[1].length);
+  const item = urlLocArr[2].split("&");
+  const itemName = item[0]
+    .split("-")
+    .join("_");
+
+  let bonusIdStr = "";
+  if (item.length > 1) {
+    bonusIdStr = ",bonus_id=" + urlLocArr[2]
+      .split("&")[1]
+      .substr(6, urlLocArr[2].split("&")[1].length)
+      .split(":")
+      .join("/");
+  }
+
+  let str = itemType + "=" + itemName + ",id=" + itemId + bonusIdStr;
+  let el = document.getElementById("simcstr");
+  el.innerHTML = str;
+}
+
+function createGearStrElement() {
+  let el = document.createElement("span");
+  el.style.color = "#ffa";
+  el.id = "simcstr";
+  document
+    .getElementsByClassName("text")[0]
+    .insertBefore(el, document.getElementsByClassName("text")[0].childNodes[0]);
+}
+
+window.onload = function () {
+  createGearStrElement();
+  generateGearStr();
+}
+
+window.onclick = function () {
+  generateGearStr();
+}
